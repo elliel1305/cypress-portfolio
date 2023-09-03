e86aa726dd9bdca42ad8039027037f2fba884405
Ellie <ellie.gludlow@gmail.com>
2023-09-03 19:35:02 +0100

    Added a function

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -12,27 +12,28 @@ describe('Test Coffee-Cart App', () => {
         cy.get('[data-cy="Espresso-Con Panna"]').click() // Click on button
         cy.get('[data-cy="Cafe-Breve"]').click() // Click on button
         cy.get('[aria-label="Cart page"]').click() // Click on button
-        cy.get('[aria-label="Cart page"]').contains('cart (9)')
-        cy.get('[data-test="checkout"]').contains('Total: $119.00')
-        cy.get('.list-item').first().contains('Americano')
-        cy.get('.list-item').as("cartItem")
-        cy.get('@cartItem').eq(1).contains('Cafe Breve')
-        cy.get('.list-item').eq(2).contains('Cafe Latte')
-        cy.get('.list-item').eq(3).contains('Cappuccino')
-        cy.get('.list-item').eq(4).contains('Espresso')
-        cy.get('.list-item').eq(5).contains('Espresso Con Panna')
-        cy.get('.list-item').eq(6).contains('Espresso Macchiato')
-        cy.get('.list-item').eq(7).contains('Flat White')
-        cy.get('.list-item').eq(8).contains('Mocha')
-        cy.get('.list-item').first().get(".unit-desc").contains('7')
-        cy.get('.list-item').eq(1).get(".unit-desc").contains('15')
-        cy.get('.list-item').eq(2).get(".unit-desc").contains('16')
-        cy.get('.list-item').eq(3).get(".unit-desc").contains('19')
-        cy.get('.list-item').eq(4).get(".unit-desc").contains('10')
-        cy.get('.list-item').eq(5).get(".unit-desc").contains('14')
-        cy.get('.list-item').eq(6).get(".unit-desc").contains('12')
-        cy.get('.list-item').eq(7).get(".unit-desc").contains('18')
-        cy.get('.list-item').eq(8).get(".unit-desc").contains('8')
+        cy.get('[aria-label="Cart page"]').contains('cart (10)')
+        cy.get('[data-test="checkout"]').contains('Total: $133.00')
+        getCartItem().first().contains('Americano')
+        getCartItem().as("cartItem")
+
+      getCartItem().eq(1).contains('Cafe Breve')
+        getCartItem().eq(2).contains('Cafe Latte')
+        getCartItem().eq(3).contains('Cappuccino')
+        getCartItem().eq(4).contains('Espresso')
+        getCartItem().eq(5).contains('Espresso Con Panna')
+        getCartItem().eq(6).contains('Espresso Macchiato')
+        getCartItem().eq(7).contains('Flat White')
+        getCartItem().eq(8).contains('Mocha')
+        getCartItem().first().get(".unit-desc").contains('7')
+        getCartItem().eq(1).get(".unit-desc").contains('15')
+        getCartItem().eq(2).get(".unit-desc").contains('16')
+        getCartItem().eq(3).get(".unit-desc").contains('19')
+        getCartItem().eq(4).get(".unit-desc").contains('10')
+        getCartItem().eq(5).get(".unit-desc").contains('14')
+        getCartItem().eq(6).get(".unit-desc").contains('12')
+        getCartItem().eq(7).get(".unit-desc").contains('18')
+        getCartItem().eq(8).get(".unit-desc").contains('8')
         cy.get('[aria-label="Proceed to checkout"]').click() // Click on button
         cy.get('[for="name"]').type('Ellie')
         cy.get('[for="email"]').type('ellie.gludlow@gmail.com')
@@ -42,3 +43,7 @@ describe('Test Coffee-Cart App', () => {
 
     })
 })
+
+function getCartItem (){
+    return cy.get('.list-item')
+}
